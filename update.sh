b5a71209cb5042ec61f04cf53917fe681bfe4857
Nikhil <nityagi@akamai.com>
2021-06-22 08:14:53 +0000

    added update.sh for new deb packages

---
+++ update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+set -v
+
+export KEYNAME=nityagi@akamai.com
+
+(
+    set -e
+    set -v
+
+    cd ./ubuntu/
+
+    # Packages & Packages.gz
+    dpkg-scanpackages --multiversion . > Packages
+    gzip -k -f Packages
+
+    # Release, Release.gpg & InRelease
+    apt-ftparchive release . > Release
+    gpg --default-key "${KEYNAME}" -abs -o - Release > Release.gpg
+    gpg --default-key "${KEYNAME}" --clearsign -o - Release > InRelease
+)
